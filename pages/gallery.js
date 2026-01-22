51a4989eff76850cbb912727cd996a67db10ba65
Prashant Raj <rajprashant3302@gmail.com>
2026-01-23 00:39:22 +0530

    Change YouTube links to embed format
    Updated YouTube links to use embed format.

--- pages/gallery.js
+++ pages/gallery.js
@@ -26,8 +26,8 @@ export default function GalleryPage() {
 
   // ✅ Put your youtube links here
   const youtubeLinks = [
-    "https://www.youtube.com/watch?v=S-ukmg7hPnk",
-    "https://www.youtube.com/watch?v=FSBZHSo1zVw",
+    "https://www.youtube.com/embed/A-5KI2vCuZI?si=6c4y6Hb8b_iumBmG",
+    "https://www.youtube.com/embed/A-5KI2vCuZI?si=6c4y6Hb8b_iumBmG",
   ];
 
   return (
